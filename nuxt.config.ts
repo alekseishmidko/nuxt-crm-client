59a686f55962fdfdca97aed435997c81d888ddff
alekseishmidko <118507792+alekseishmidko@users.noreply.github.com>
2024-07-06 23:06:46 +0300

    fin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,34 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
   devtools: { enabled: false },
-  modules: ["@nuxt/image", "@nuxtjs/tailwindcss"],
+  modules: [
+    "@nuxt/ui",
+    "@nuxt/image",
+    "shadcn-nuxt",
+    [
+      "@nuxtjs/google-fonts",
+      {
+        families: {
+          Lato: {
+            wght: [300, 400, 700],
+            ital: [300],
+          },
+        },
+      },
+    ],
+    "nuxt-icon",
+    "@pinia/nuxt",
+    [
+      "@vee-validate/nuxt",
+      {
+        autoImports: true,
+      },
+    ],
+  ],
+  shadcn: {
+    prefix: "Ui",
+    componentDir: "./components/ui",
+  },
+  pinia: {
+    storesDirs: ["./store/**"],
+  },
 });
